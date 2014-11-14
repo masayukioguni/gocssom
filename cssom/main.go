fe77eb68c92ec04271192d45d3d2031fcf50977f
masayuki.oguni <masayuki.oguni.dev@gmail.com>
2014-11-14 14:31:52 +0900

    add main.go

--- cssom/main.go
+++ cssom/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"github.com/masayukioguni/cssom"
+	"github.com/masayukioguni/gocssom/cssom"
 )
 
 func main() {
+	css := cssom.Parse(`div .a { 
+					a: rgb(1,2,3)
+					font-size: 150% !important
+		 }`)
+	css.Print()
 
 }
